51d44ba31eb58e128c7b95ce1081b27a0c329268
joaov-007 <79527341+joaov-007@users.noreply.github.com.>
2025-06-05 18:57:26 -0300

    make one aliaas command maybe speedup
    Signed-off-by: joaov-007 <79527341+joaov-007@users.noreply.github.com.>

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,10 +1,10 @@
-alias c='clear'                                                        # clear terminal
-alias l='eza -lh --icons=auto'                                         # long list
-alias ls='eza -1 --icons=auto'                                         # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto'                                       # long list dirs
-alias la='eza -la --icons=auto'                                        # show hidden files
-alias lt='eza --icons=auto --tree'                                     # list folder as tree
-alias la='eza --icons=auto --almost-all'                               # list all files
-alias v='nvim'                                                         # short name for editor
-alias cz='chezmoi'                                                     # short name for dotfiler manager
+alias c='clear' \
+ l='eza -lh --icons=auto' \
+ ls='eza -1 --icons=auto' \
+ ll='eza -lha --icons=auto --sort=name --group-directories-first' \
+ ld='eza -lhD --icons=auto' \
+ la='eza -la --icons=auto' \
+ lt='eza --icons=auto --tree' \
+ la='eza --icons=auto --almost-all' \
+ v='nvim' \
+ cz='chezmoi'
